08de289064e048e683c242c75f8a117f3237e714
timdelisle <tg339@cornell.edu>
2016-02-01 21:05:03 -0500

    closing opening menu

---
+++ _posts/2015-01-01-getting-started.markdown
@@ -0,0 +1,16 @@
+---
+layout: post
+title:  "Getting Started with data science and machine learning"
+date:   2016-01-01 06:00:00
+category: Getting Started
+author: Tim
+---
+
+Welcome to punkstats!
+
+If you're here you're probably trying to get started with data science and machine learning. 
+
+
+[jekyll]:      http://jekyllrb.com
+[jekyll-gh]:   https://github.com/jekyll/jekyll
+[jekyll-help]: https://github.com/jekyll/jekyll-help
---
+++ javascript/events.js
@@ -0,0 +1,31 @@
+$( document ).ready(function() {
+  $(".menu-icon").click(function() {
+
+    $(".main-container").animate({
+      width: "80%"
+    }, 500, function() {
+      
+    })
+
+    $("aside").animate({
+      width: "20%",
+    }, 500, function() {
+      
+    })
+  })
+
+  $(".close-icon").click(function() {
+
+    $(".main-container").animate({
+      width: "100%"
+    }, 500, function() {
+      
+    })
+
+    $("aside").animate({
+      width: 0,
+    }, 500, function() {
+      
+    })
+  })
+})
